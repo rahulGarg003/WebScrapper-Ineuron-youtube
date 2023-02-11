39ff885e934fe427f3e551cb25cfd2154949d538
rahulGarg003 <rahul.garg9460@gmail.com>
2023-02-11 20:57:02 +0530

    first commit

---
+++ scrapIneuron.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+from functools import reduce
+
+class ScrapIneuron():
+    __url = 'https://www.ineuron.ai'
+    def __init__(self):
+        pass
+
+    def scrap(self):
+        pass
