cf2301c2e3dea4434654a28f851ac9f3e2898d0d
rahulGarg003 <rahul.garg9460@gmail.com>
2023-02-13 18:51:32 +0530

    Updated Commit

--- coursePdf.py
+++ coursePdf.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 import requests
+import os
 
 class CoursePDF():
     class _PDF(FPDF):
@@ -7,20 +8,24 @@ class CoursePDF():
             super().__init__()
         def header(self):
             self.set_font('Arial', '', 12)
-            # self.cell(0, 8, , 0, 1, 'C')
-            # url = r'https://ineuron.ai/images/ineuron-logo.png'
-            # img = open('logo.png', 'wb')
-            # img.write(requests.get(url).content)
+            if(not os.path.exists('./logo.png')):
+                url = r'https://ineuron.ai/images/ineuron-logo.png'
+                img = open('logo.png', 'wb')
+                img.write(requests.get(url).content)
             self.image('logo.png', w=40, h=20, type='PNG',x=210/2-20)
             self.cell(w=0,border=True)
 
         def footer(self):
             self.set_y(-15)
             self.set_font('Arial', '', 9)
+            self.cell(w=0,border=True)
             self.cell(0, 8, f'Page {self.page_no()}', 0, 0, 'C')
 
     def __init__(self):
         pass
+
+    def __formatText(self, txt):
+        return str(txt).encode('latin-1', 'replace').decode('latin-1')
 
     def generateCoursePdf(self,data: dict):
         # cell height
@@ -29,89 +34,81 @@ class CoursePDF():
         pdf.add_page()
         pdf.ln(4)
         pdf.set_font('Arial', 'BU', 16)
-        pdf.multi_cell(w=0, h=5, txt=data['title'], align='C')
+        pdf.multi_cell(w=0, h=5, txt=self.__formatText(data.get('title','')), align='C')
         pdf.ln(4)
         pdf.set_font('Arial', '', 9)
-        pdf.multi_cell(w=0, h=5, txt=data['details']['description'].encode('latin-1', 'replace').decode('latin-1'))
+        pdf.multi_cell(w=0, h=5, txt=self.__formatText(data.get('details',{}).get('description','')))
         pdf.set_font('Arial', 'B', 9)
-        pdf.cell(w=210/3, h=ch, txt=f"Mode: {data['details']['mode']}", ln=0)
-        pdf.cell(w=210/3, h=ch, txt=f"Language: {data['meta']['overview']['language']}", ln=0)
-        pdf.cell(w=210/3, h=ch, txt=f"Duration: {data['meta']['duration']}", ln=1)
+        pdf.cell(w=210/3, h=ch, txt=f"Mode: {self.__formatText(data.get('details','').get('mode'))}", ln=0)
+        pdf.cell(w=210/3, h=ch, txt=f"Language: {self.__formatText(data.get('meta',{}).get('overview',{}).get('language',''))}", ln=0)
+        pdf.cell(w=210/3, h=ch, txt=f"Duration: {self.__formatText(data.get('meta',{}).get('duration',''))}", ln=1)
 
-        y_pos = 65
+        # y_pos = 65
 
-        pdf.set_y(y_pos)
+        # pdf.set_y(y_pos)
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="What you'll learn:", ln=1)
         pdf.set_font('Arial', '', 9)
-        for d in data['meta']['overview']['learn']:
-            pdf.cell(w=30, h=ch, txt=f'{" "*4}{d}', ln=1)
-        pdf.ln(4)
+        for d in data.get('meta',{}).get('overview',{}).get('learn',[]):
+            pdf.cell(w=30, h=ch, txt=f'{" "*4}{self.__formatText(d)}', ln=1)
+        pdf.ln(2)
 
-        pdf.set_xy(210/3, y_pos)
+        # pdf.set_xy(210/3, y_pos)
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="Course Features:", ln=1)
         pdf.set_font('Arial', '', 9)
-        for feature in data['meta']['overview']['features']:
-            pdf.set_x(210/3)
-            pdf.cell(w=30, h=ch, txt=f'{" "*4}{feature}', ln=1)
-        pdf.ln(4)
+        for feature in data.get('meta',{}).get('overview',{}).get('features',[]):
+            # pdf.set_x(210/3)
+            pdf.cell(w=30, h=ch, txt=f'{" "*4}{self.__formatText(feature)}', ln=1)
+        pdf.ln(2)
 
-        pdf.set_xy(210*2/3,y_pos)
+        # pdf.set_xy(210*2/3,y_pos)
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="Requirements:", ln=1)
         pdf.set_font('Arial', '', 9)
-        for d in data['meta']['overview']['requirements']:
-            pdf.set_x(210*2/3)
-            pdf.cell(w=30, h=ch, txt=f'{" "*4}{d}', ln=1)
-        pdf.ln(4)
+        for d in data.get('meta',{}).get('overview',{}).get('requirements',[]):
+            # pdf.set_x(210*2/3)
+            pdf.cell(w=30, h=ch, txt=f'{" "*4}{self.__formatText(d)}', ln=1)
+        pdf.ln(2)
 
-        pdf.set_x(210*2/3)
+        # pdf.set_x(210*2/3)
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="Pricing:", ln=1)
         pdf.set_font('Arial', '', 9)
-        pdf.set_x(210*2/3)
-        pdf.cell(w=0, h=ch, txt=f"{' '*4}{data['details']['pricing']['IN']} INR", ln=1)
-        pdf.set_x(210*2/3)
-        pdf.cell(w=0, h=ch, txt=f"{' '*4}{data['details']['pricing']['US']} USD", ln=1)
-
-        pdf.set_y(y_pos+(15*ch))
+        # pdf.set_x(210*2/3)
+        pdf.cell(w=0, h=ch, txt=f"{' '*4}{self.__formatText(data.get('details',{}).get('pricing',{}).get('IN',''))} INR", ln=1)
+        # pdf.set_x(210*2/3)
+        pdf.cell(w=0, h=ch, txt=f"{' '*4}{self.__formatText(data.get('details',{}).get('pricing',{}).get('US',''))} USD", ln=1)
+        pdf.ln(2)
+        # pdf.set_y(y_pos+(15*ch))
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="Course Curriculum:", ln=1)
         pdf.set_font('Arial', '', 9)
-        for num, d in enumerate(data['meta']['curriculum'].values()):
+        for num, d in enumerate(data.get('meta',{}).get('curriculum',{}).values()):
             pdf.set_font('Arial', 'B', 9)
-            pdf.cell(w=0, h=ch, txt='{}{}. {}'.format(" "*4,num+1, d['title'].encode('latin-1', 'replace').decode('latin-1')), ln=1)
+            pdf.cell(w=0, h=ch, txt='{}{}. {}'.format(" "*4,num+1, self.__formatText(d.get('title',''))), ln=1)
             for item in d['items']:
                 pdf.set_font('Arial', '', 9)
-                pdf.cell(w=0, h=ch, txt=f"{' '*12}{item['title'].encode('latin-1', 'replace').decode('latin-1')}", ln=1)
-        pdf.ln(4)
+                pdf.cell(w=0, h=ch, txt=f"{' '*12}{self.__formatText(item.get('title'))}", ln=1)
+        pdf.ln(2)
 
         pdf.set_font('Arial', 'B', 10)
         pdf.cell(w=30, h=ch, txt="Instructors:", ln=1)
         # pdf.set_font('Arial', '', 12)
-        for num, d in enumerate(data['meta']['instructordetails'].values()):
+        for num, d in enumerate(data.get('meta',{}).get('instructordetails',{}).values()):
             pdf.set_font('Arial', 'B', 10)
-            pdf.cell(w=0, h=ch, txt='{}. {}'.format(num+1, d['name']), ln=1)
+            pdf.cell(w=0, h=ch, txt='{}. {}'.format(num+1, self.__formatText(d.get('name',''))), ln=1)
             pdf.set_font('Arial', '', 9)
-            pdf.multi_cell(w=0, h=5, txt=d['description'].encode('latin-1', 'replace').decode('latin-1'))
+            pdf.multi_cell(w=0, h=5, txt=self.__formatText(d.get('description','')))
             pdf.set_font('Arial', '', 9)
             pdf.cell(w=30, h=ch, txt=f"{' '*8}Email: ", ln=0)
-            pdf.cell(w=100, h=ch, txt=d['email'], ln=1)
-            for key, value in d['social'].items():
-                pdf.cell(w=30, h=ch, txt=f"{' '*8}{key}: ", ln=0)
-                pdf.cell(w=100, h=ch, txt=value, ln=1)
-        pdf.ln(4)
+            pdf.cell(w=100, h=ch, txt=self.__formatText(d.get('email','')), ln=1)
+            for key, value in d.get('social',{}).items():
+                pdf.cell(w=30, h=ch, txt=f"{' '*8}{self.__formatText(key)}: ", ln=0)
+                pdf.cell(w=100, h=ch, txt=self.__formatText(value), ln=1)
+        # pdf.ln(4)
 
-        # pdf.cell(w=30, h=ch, txt="Date: ", ln=0)
-        # pdf.cell(w=30, h=ch, txt="01/01/2022", ln=1)
-        # pdf.cell(w=30, h=ch, txt="Author: ", ln=0)
-        # pdf.cell(w=30, h=ch, txt="Max Mustermann", ln=1)
-        # pdf.ln(ch)
-        # pdf.multi_cell(w=0, h=5, txt="lorem.paragraph()")
-        # pdf.ln(ch)
-        # pdf.multi_cell(w=0, h=5, txt="lorem.paragraph()")
-        pdf.output(f'{data["title"].replace(" ","-")}.pdf', 'F')
+        pdf.output(f'./pdfs/{data.get("title","testpdf").replace(" ","-")}.pdf', 'F')
 
 if __name__ == '__main__':
     CoursePDF().generateCoursePdf()
--- scrapIneuron.py
+++ scrapIneuron.py
@@ -10,9 +10,9 @@ class ScrapIneuron():
             url = 'https://www.ineuron.ai'
             rawdata = bs(requests.get(url).content, 'html.parser')
             data = json.loads(rawdata.find_all('script', id = '__NEXT_DATA__')[0].text)
-            self.__categories = data['props']['pageProps']['initialState']['init']['categories']
-            self.__courses = data['props']['pageProps']['initialState']['init']['courses']
-            self.__instructors = data['props']['pageProps']['initialState']['init']['instructors']
+            self.__categories = data.get('props',{}).get('pageProps',{}).get('initialState',{}).get('init',{}).get('categories',{})
+            self.__courses = data.get('props',{}).get('pageProps',{}).get('initialState',{}).get('init',{}).get('courses',{})
+            self.__instructors = data.get('props',{}).get('pageProps',{}).get('initialState',{}).get('init',{}).get('instructors',{})
         except Exception as e:
             raise Exception('Something went worng with Ineuron Website!!!')
 
@@ -21,8 +21,8 @@ class ScrapIneuron():
         coursesByCategory = {}
         for key, value in self.__courses.items():
             value['title'] = key
-            if(value['categoryId'] in coursesByCategory):
-                coursesByCategory[value['categoryId']].append(value)
+            if(value.get('categoryId','') in coursesByCategory):
+                coursesByCategory.get(value['categoryId'],'').append(value)
             else:
                 coursesByCategory[value['categoryId']] = [value]
         return {"categories": self.__categories, "courses": coursesByCategory, "instructor": self.__instructors}
@@ -36,12 +36,12 @@ class ScrapIneuron():
             url = f"https://www.ineuron.ai/course/{courseTitle.replace(' ', '-')}"
             rawdata = bs(requests.get(url).content, 'html.parser')
             data = json.loads(rawdata.find_all('script', id = '__NEXT_DATA__')[0].text)
-            course = data['props']['pageProps']['data']
-            instructors = data['props']['pageProps']['initialState']['init']['instructors']
-            courseInstructor = course['meta']['instructors']
+            course = data.get('props',{}).get('pageProps',{}).get('data',{})
+            instructors = data.get('props',{}).get('pageProps',{}).get('initialState',{}).get('init',{}).get('instructors',{})
+            courseInstructor = course.get('meta',{}).get('instructors',{})
             courseInstructorDetails = {}
             for instruc in courseInstructor:
-                courseInstructorDetails[instruc] = instructors[instruc]
+                courseInstructorDetails[instruc] = instructors.get(instruc,{})
             course['meta']['instructordetails'] = courseInstructorDetails
             return course
         except Exception as e:
@@ -50,5 +50,9 @@ class ScrapIneuron():
 if __name__ == '__main__':
     obj = ScrapIneuron()
     pdf = CoursePDF()
-    # obj.getCourseDetails('Full-Stack-Web-Development-with-Python-in-Hindi-Tech-Neuron')
-    pdf.generateCoursePdf(obj.getCourseDetails('Full-Stack-Web-Development-with-Python-in-Hindi-Tech-Neuron'))
+    # pdf.generateCoursePdf(obj.getCourseDetails('Automatic Number Plate Recognition'))
+    courses = obj.getListofCourses()
+    # for crs in courses:
+    #     pdf.generateCoursePdf(obj.getCourseDetails(crs))
+    #     print(f"Pdf generateed for: {crs}")
+    print(len(courses))
